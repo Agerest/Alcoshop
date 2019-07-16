ae5dc0f6dea2253eadad353560808e55c1621224
Сергей Владимирович Крес <160257@edu.psuti.ru>
2019-07-16 23:02:46 +0400

    hello page fix

--- src/main/java/alco/shop/htmlpagecont/PageController.java
+++ src/main/java/alco/shop/htmlpagecont/PageController.java
@@ -2,14 +2,15 @@ package alco.shop.htmlpagecont;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PageController {
 
-    @GetMapping("")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String hello(Model model) {
-        return "hello";
+        return "index";
     }
 
 }

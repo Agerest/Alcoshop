9011ebeb0d902e1c69cf869c6c31fc6b9d150ac4
Дмитрий <balalajkalodocnik@gmail.com>
2019-07-19 18:36:30 +0400

    Третий тест

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -2,10 +2,10 @@
 
 function getJson() {
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "/getdrinks", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.open("GET", "/getdrinks", true);
+    // xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send();
-    console.log("Test 2");
+    console.log("Test 3");
     if (xhr.status == 200) {
         console.log("JSON " + xhr.responseText);
     } else {

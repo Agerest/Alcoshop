a76f33205bc2c2326839b326cbc262ec90f641b8
Дмитрий <balalajkalodocnik@gmail.com>
2019-07-19 23:50:31 +0400

    testreact

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -6,11 +6,11 @@ function getDrinks() {
 }
 
 function createDrink(drink) {
-    return (<article className="product col m-3" id="${drink.id}">
-        <a href="" className="product-name"><h3>${drink.name}</h3></a>
+    return (<article className="product col m-3" id="{drink.id}">
+        <a href="" className="product-name"><h3>{drink.name}</h3></a>
         <a href=""><img className="product-img" src="../img/alco/beer.png" alt="alco-img" /></a>
         <h4>&#9733; &#9733; &#9733; &#9733; &#9734;</h4>
-        <p className="description">${drink.description}</p>
+        <p className="description">{drink.description}</p>
         <p>Крепость: 4.7%</p>
     </article>)
 }

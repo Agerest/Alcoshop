7b60a0c2db7885354fe04a57e78db7d32b80981b
Дмитрий <balalajkalodocnik@gmail.com>
2019-07-19 23:36:19 +0400

    test react js

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -1,11 +1,25 @@
-getJson();
-
-function getJson() {
+function getDrinks() {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "/getdrinks", false);
-    // xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send();
-    console.log("Test 4");
-    let drinks = JSON.parse(xhr.responseText)
-    console.log(drinks[0].name);
+    return JSON.parse(xhr.responseText)
 }
+
+function createDrink(drink) {
+    return (<article className="product col m-3" id="${drink.id}">
+        <a href="" className="product-name"><h3>${drink.name}</h3></a>
+        <a href=""><img className="product-img" src="../img/alco/beer.png" alt="alco-img" /></a>
+        <h4>&#9733; &#9733; &#9733; &#9733; &#9734;</h4>
+        <p className="description">${drink.description}</p>
+        <p>Крепость: 4.7%</p>
+    </article>)
+}
+
+let drinks = getDrinks();
+let htmlDrinks;
+
+drinks.array.forEach(element => {
+    htmlDrinks += createDrink(element) + " ";
+});
+
+ReactDOM.render(htmlDrinks, document.getElementById("root-main"));
